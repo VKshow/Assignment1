ddd5874b6b4ce7f0029dbc5a330547564c64d124
vk <justvk2013@gmail.com>
2023-11-19 16:24:13 -0700

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 
 
 function App() {
-  
+  const [counter, setCounter] = useState(0);
+
+  const resetCounter = () => {
+    setCounter(0);
+  }
+
+  useEffect(()=>{
+    const counterIncr = setInterval(()=> {
+      setCounter((prevCounter)=>{
+        console.log("useEffect starting")
+        return prevCounter + 1
+      })
+    },3000);
+
+    return () =>  clearInterval(counterIncr);
+  }, [])
 
   return (
-    <>
-      
-    </>
+    <div className='container'>
+      <div className="counter">{counter}</div>
+      <div className="btn-container">
+        <button 
+        className='btn'
+        onClick={resetCounter}>reset</button>
+      </div>
+    </div>
+    
   )
 }
 
