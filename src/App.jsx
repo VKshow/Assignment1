55e7f5e179c28b520dc40cce67051529b8cba72f
vk <justvk2013@gmail.com>
2023-11-19 17:00:50 -0700

    second branch done

--- src/App.jsx
+++ src/App.jsx
@@ -4,39 +4,53 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const [running. setRunning] = useState(false)
+  const [running, setRunning] = useState(false);
+  
 
   const resetCounter = () => {
     setCounter(0);
   }
 
   useEffect(()=>{
-    const counterIncr = setInterval(()=> {
-      setCounter((prevCounter)=>{
-        console.log("useEffect starting")
-        return prevCounter + 1
-      })
-    },3000);
+    if (running){
+      const counterIncr = setInterval(()=> {
+      
+        setCounter((prevCounter)=>{
+          console.log("useEffect starting")
+          return prevCounter + 1
+        })
+      },3000);
+  
+      return () =>  clearInterval(counterIncr);
+    }
+    
+  }, [running]);
 
-    return () =>  clearInterval(counterIncr);
-  }, [])
+  const startCounter = ()=>{
+    setRunning(true);
+  }
+
+  const stopCounter = ()=>{
+    setRunning(false);
+  }
 
   return (
     <div className='container'>
       <div className="counter">{counter}</div>
       <div className="btn-container">
         <button 
-        className='btn'
-        onClick={resetCounter}>reset</button>
+        className='btn btn-start'
+        onClick={startCounter}>start</button>
 
         <button 
         className='btn'
         onClick={resetCounter}>reset</button>
 
         <button 
-        className='btn'
-        onClick={resetCounter}>reset</button>
+        className='btn btn-stop'
+        onClick={stopCounter}>stop</button>
       </div>
+      
     </div>
     
   )
